99bd070ed68dfe827406122e762017ad4cc8befb
The-C2020 <chris11235@hotmail.com>
2021-09-22 17:44:35 +0200

    third commit

--- index.js
+++ index.js
@@ -1,193 +1,62 @@
-/*
+let currentRound = 0;
+let totalRounds = document.querySelectorAll(".quiz-container").length;
+let correctAnswers = 0;
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
+const allSections = document.querySelectorAll(".quiz-container");
+const allNextButtons = document.querySelectorAll(".btn-next");
 
-var x = canvas.width / 2;
-var y = canvas.height - 30;
-var dx = 2;
-var dy = -2;
-var ballRadius = 10;
+document.querySelectorAll(".btn-next").forEach((button) => {
+  button.addEventListener("click", () => {
+    getNextSection();
+  });
+});
 
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width - paddleWidth) / 2;
-
-var rightPressed = false;
-var leftPressed = false;
-
-var brickRowCount = 3;
-var brickColumnCount = 5;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
-var bricks = [];
-for (var c = 0; c < brickColumnCount; c++) {
-  bricks[c] = [];
-  for (var r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
-  }
-}
-
-var score = 0;
-var lives = 3;
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-
-function drawBricks() {
-  for (var c = 0; c < brickColumnCount; c++) {
-    for (var r = 0; r < brickRowCount; r++) {
-      if (bricks[c][r].status == 1) {
-        var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
-        var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
-}
-
-function drawScore() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText("Score: " + score, 8, 20);
-}
-
-function drawLives() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText("Lives: " + lives, canvas.width - 65, 20);
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBricks();
-  drawBall();
-  drawPaddle();
-  drawScore();
-  drawLives();
-  collisionDetection();
-  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if (y + dy < ballRadius) {
-    dy = -dy;
-  } else if (y + dy > canvas.height - ballRadius) {
-    if (x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
-    } else {
-      lives--;
-      if (!lives) {
-        alert("GAME OVER");
-        document.location.reload();
-      } else {
-        x = canvas.width / 2;
-        y = canvas.height - 30;
-        dx = 2;
-        dy = -2;
-        paddleX = (canvas.width - paddleWidth) / 2;
-      }
-    }
-  }
-
-  if (rightPressed) {
-    paddleX += 7;
-    if (paddleX + paddleWidth > canvas.width) {
-      paddleX = canvas.width - paddleWidth;
-    }
-  }
-  else if (leftPressed) {
-    paddleX -= 7;
-    if (paddleX < 0) {
-      paddleX = 0;
-    }
-  }
-
-  x += dx;
-  y += dy;
-
-  requestAnimationFrame(draw);
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-  if (e.key == "Right" || e.key == "ArrowRight") {
-    rightPressed = true;
-  }
-  else if (e.key == "Left" || e.key == "ArrowLeft") {
-    leftPressed = true;
-  }
-}
-
-function keyUpHandler(e) {
-  if (e.key == "Right" || e.key == "ArrowRight") {
-    rightPressed = false;
-  }
-  else if (e.key == "Left" || e.key == "ArrowLeft") {
-    leftPressed = false;
-  }
-}
-
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
-function mouseMoveHandler(e) {
-  var relativeX = e.clientX - canvas.offsetLeft;
-  if (relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth / 2;
-  }
-}
-
-function collisionDetection() {
-  for (var c = 0; c < brickColumnCount; c++) {
-    for (var r = 0; r < brickRowCount; r++) {
-      var b = bricks[c][r];
-      if (b.status == 1) {
-        if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
-
-          dy = -dy;
-          b.status = 0;
-          score++;
-          if (score == brickRowCount * brickColumnCount) {
-            alert("YOU WIN, CONGRATULATIONS!");
-            document.location.reload();
+document.querySelectorAll(".btn-answer").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (questionAnswered()) {
+      let allAnswers = document.querySelectorAll(
+        `.question-${currentRound + 1}`
+      );
+      allNextButtons[currentRound].classList.remove("hidden");
+      allAnswers.forEach((item) => {
+        if (item.dataset.solution === "correct") {
+          item.parentElement.classList.add("correct-answer");
+          if (item.checked) {
+            correctAnswers++;
+            document
+              .querySelectorAll(".solution-box__correct")
+              [currentRound].classList.remove("hidden");
+          } else {
+            document
+              .querySelectorAll(".solution-box__wrong")
+              [currentRound].classList.remove("hidden");
           }
+        } else {
+          item.parentElement.classList.add("wrong-answer");
         }
-      }
+      });
     }
+  });
+});
+
+const getNextSection = () => {
+  allSections[currentRound].classList.add("hidden");
+  currentRound++;
+  currentRound === 3
+    ? showEndScreen()
+    : allSections[currentRound].classList.remove("hidden");
+};
+
+const showEndScreen = () => {
+  document.getElementById("correct-answers").innerHTML = correctAnswers;
+  document.querySelector(".result-container").classList.remove("hidden");
+};
+
+const questionAnswered = () => {
+  let allAnswers = document.querySelectorAll(`.question-${currentRound + 1}`);
+  console.log(allAnswers);
+  for (let i = 0; i < allAnswers.length; i++) {
+    if (allAnswers[i].checked) return true;
   }
-}
-
-//draw();
-
-*/
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = `${slider.value} %`;
-
-slider.oninput = function () {
-  output.innerHTML = `${this.value} %`;
+  return false;
 };
